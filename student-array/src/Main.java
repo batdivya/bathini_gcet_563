012c39ff1ff1d4000107e21ea7dc98df557dc46d
batdivya <31980319+batdivya@users.noreply.github.com>
2017-09-15 11:55:54 +0530

    added content

---
+++ student-array/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		student st=new student();
+		studentGroup sg=new studentGroup();
+		
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
